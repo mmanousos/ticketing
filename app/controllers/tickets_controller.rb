f7b5a1fb1fac83f66f344bd42a9da8dab2de6dfb
Melissa Manousos <mmanousos@gmail.com>
2020-01-12 15:17:59 -0800

    Adds ticket functionality

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,6 @@
 class TicketsController < ApplicationController
+  before_action :set_ticket, only: [:edit, :show, :destroy]
+
   def index 
     @tickets = Ticket.all
   end
@@ -7,23 +9,44 @@ class TicketsController < ApplicationController
   end
 
   def new
-
+    @ticket = Ticket.new
   end  
 
   def edit
-
   end  
 
   def create
+    @ticket = Ticket.new(ticket_params)
 
+    if @ticket.save 
+      flash[:notice] = "Ticket was successfully created."
+      redirect_to tickets_path
+    else 
+      render :new
+    end
   end  
 
   def update 
-
+   if ticket.update(ticket_params)
+    flash[:notice] = "Ticket was successfully updated."
+    redirect_to ticket_path(@ticket)
+   else
+    render :edit
+   end
   end  
 
   def destroy
-
+    @ticket.destroy
+    redirect_to tickets_path
   end  
+
+  private
+  def ticket_params
+    params.require(:ticket).permit(:project_id, :name, :body, :status)
+  end
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def status_options_for_select
+    status_options = %w(new in-progress blocked fixed)
+    status_options.map { |status| [status.capitalize, status] }
+  end
+
+  def projects_for_select
+    Project.all.map {|p| [p.name, p.id] } 
+  end
 end
